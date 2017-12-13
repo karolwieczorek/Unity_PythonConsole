960cf53d3656f2c43dde60906789f0432ff0b53b
Karl <karolwieczorek9@o2.pl>
2017-12-13 01:35:05 +0100

    resize console input & output

--- UnityPython/Assets/Examples/Resizer.cs
+++ UnityPython/Assets/Examples/Resizer.cs
@@ -1,7 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Resizer : MonoBehaviour {
-    
+public class Resizer : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    public float minSize;
+    public RectTransform resizedTransform;
+
+    Vector3 prevMousePos;
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        prevMousePos = Input.mousePosition;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        var mouseMovmentY = Input.mousePosition.y - prevMousePos.y;
+        ChangeRectHeight(mouseMovmentY);
+
+        prevMousePos = Input.mousePosition;
+    }
+
+    private void ChangeRectHeight(float mouseMovmentY) {
+        var size = resizedTransform.sizeDelta;
+        size.y = Mathf.Max(size.y + mouseMovmentY, minSize);
+        resizedTransform.sizeDelta = size;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+    }
 }
